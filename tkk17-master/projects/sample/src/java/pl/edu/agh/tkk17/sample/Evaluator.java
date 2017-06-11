4d7f15c30e516769339cc5bfc3088f8da4cb5fba
Piotr <boskislowik@wp.pl>
2017-06-11 02:43:02 +0200

    init

---
+++ tkk17-master/projects/sample/src/java/pl/edu/agh/tkk17/sample/Evaluator.java
@@ -0,0 +1,18 @@
+package pl.edu.agh.tkk17.sample;
+
+public class Evaluator
+{
+    public static float evaluate(Node tree)
+    {
+        return evaluateRpn(tree);
+    }
+
+    private static float evaluateRpn(Node node)
+    {
+        RpnEvaluatorVisitor visitor = new RpnEvaluatorVisitor();
+        node.accept(visitor);
+
+        return visitor.getValue();
+    }
+
+}
